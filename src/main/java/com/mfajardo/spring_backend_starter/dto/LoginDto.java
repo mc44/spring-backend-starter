86368a3761eb45020402bad35afd7adc7acf2b44
mc44 <marcfajardo14@gmail.com>
2026-02-07 12:33:56 +0800

    dtos

---
+++ src/main/java/com/mfajardo/spring_backend_starter/dto/LoginDto.java
@@ -0,0 +1,9 @@
+package com.mfajardo.spring_backend_starter.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/mfajardo/spring_backend_starter/dto/RefreshTokenDto.java
@@ -0,0 +1,8 @@
+package com.mfajardo.spring_backend_starter.dto;
+
+import lombok.Data;
+
+@Data
+public class RefreshTokenDto {
+    private String refreshToken;
+}
